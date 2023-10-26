a12cf98b15a4259b3c996cb65a1cfe6d43db7df8
Bhanu-0M <118541518+Bhanu-0M@users.noreply.github.com>
2023-10-26 17:38:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function generate() {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+ 
+    for (let i = 1; i <= 8; i++) {
+        let char = Math.floor(Math.random()
+            * str.length + 1);
+ 
+        pass += str.charAt(char)
+    }
+ 
+    document.getElementById("gnrtd").innerHTML=pass;
+}
+ 
